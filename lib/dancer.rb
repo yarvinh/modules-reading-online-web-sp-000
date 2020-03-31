e10f90bdbd2a48bbe765f99b2e3d024ded53a561
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-31 22:47:03 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,8 +3,8 @@ require_relative './dance_module.rb'
 
 class Dancer
      include Dance
-extend FancyDance::ClassMethods
-include FancyDance::InstanceMethods
+# extend FancyDance::ClassMethods
+# include FancyDance::InstanceMethods
   attr_accessor :name
   def initialize(name)
     @name = name
