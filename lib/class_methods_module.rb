9cdeb3b71e4eebfa1c6d053216237fa483fa6c1d
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-31 22:51:48 +0000

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,14 @@
+module MetaDancing
+
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
+
+class Dancer
+  extend MetaDancing
+end
+
+class Kid
+  extend MetaDancing
+end
